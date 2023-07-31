7689be159962079a0c6d3bf58b9adaba855f98ed
Prince <pri4cer@gmail.com>
2023-07-31 22:32:21 +0530

    Update app.js to match frontend paths

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,10 @@ const cors = require('cors');
 const app = express();
 // Enable CORS for all routes
 app.use(cors());
-app.use('/clips', express.static(path.join(__dirname, 'clips')));
+app.use('/api/clips', express.static(path.join(__dirname, 'clips')));
 
 // Serve static files from the "thumbnail" folder
-app.use('/thumbnails', express.static(path.join(__dirname, 'thumbnails')));
+app.use('/api/thumbnails', express.static(path.join(__dirname, 'thumbnails')));
 
 app.use('/api/v1', searchRouter);
 
